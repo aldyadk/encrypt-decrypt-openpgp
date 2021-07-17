3f9c19779456f6f0f116495ff5671a8626cf57e7
Aldy ADK Andika <aldy.andika@gmail.com>
2021-07-17 17:26:00 +0700

    edit decryptKey

--- encrypt-decrypt.js
+++ encrypt-decrypt.js
@@ -2,29 +2,34 @@ const openpgp = require('openpgp');
 const fs = require('fs/promises');
 
 (async () => {
-  const publicKeyArmored = await fs.readFile('./public.asc', {encoding: 'utf-8'});
-  const privateKeyArmored = await fs.readFile('./private.asc', {encoding: 'utf-8'});
-  const passphrase = 'super long and hard to guess secret';
+  try {
+    const publicKeyArmored = await fs.readFile('./public.asc', { encoding: 'utf-8' });
+    const privateKeyArmored = await fs.readFile('./private.asc', { encoding: 'utf-8' });
+    const passphrase = 'super long and hard to guess secret';
 
-  const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
+    const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
 
-  const privateKey = await openpgp.decryptKey({
-    privateKey: await openpgp.readKey({ armoredKey: privateKeyArmored }),
-    passphrase
-  });
+    const privateKey = await openpgp.decryptKey({
+      privateKey: await openpgp.readKey({ armoredKey: privateKeyArmored }),
+      passphrase,
+      config: { v5Keys: true }
+    });
 
-  const encrypted = await openpgp.encrypt({
-    message: await openpgp.createMessage({ text: await fs.readFile('./test.txt', { encoding: 'utf8' }) }),
-    encryptionKeys: publicKey,
-  });
-  console.log(encrypted);
+    const encrypted = await openpgp.encrypt({
+      message: await openpgp.createMessage({ text: await fs.readFile('./test.txt', { encoding: 'utf8' }) }),
+      encryptionKeys: publicKey
+    });
+    console.log(encrypted);
 
-  const message = await openpgp.readMessage({
-    armoredMessage: encrypted
-  });
-  const { data: decrypted, signatures } = await openpgp.decrypt({
-    message,
-    decryptionKeys: privateKey
-  });
-  console.log(decrypted);
+    const message = await openpgp.readMessage({
+      armoredMessage: encrypted
+    });
+    const { data: decrypted, signatures } = await openpgp.decrypt({
+      message,
+      decryptionKeys: privateKey
+    });
+    console.log(decrypted);
+  } catch (error) {
+    console.log(error)
+  }
 })();
